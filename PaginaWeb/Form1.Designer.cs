2cae65682601991b0531415f409633496e6cd678
Pabloricalderon <112725005+Pabloricalderon@users.noreply.github.com>
2024-02-08 09:20:43 -0600

    Programa completo

--- PaginaWeb/Form1.Designer.cs
+++ PaginaWeb/Form1.Designer.cs
@@ -36,7 +36,9 @@
             this.btnIr = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -47,7 +49,7 @@
             this.navegarToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1413, 33);
+            this.menuStrip1.Size = new System.Drawing.Size(1413, 36);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -64,29 +66,29 @@
             // inicioToolStripMenuItem
             // 
             this.inicioToolStripMenuItem.Name = "inicioToolStripMenuItem";
-            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(229, 34);
             this.inicioToolStripMenuItem.Text = "Inicio";
             this.inicioToolStripMenuItem.Click += new System.EventHandler(this.inicioToolStripMenuItem_Click);
             // 
             // haciaAtrasToolStripMenuItem
             // 
             this.haciaAtrasToolStripMenuItem.Name = "haciaAtrasToolStripMenuItem";
-            this.haciaAtrasToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.haciaAtrasToolStripMenuItem.Size = new System.Drawing.Size(229, 34);
             this.haciaAtrasToolStripMenuItem.Text = "Hacia atras";
             this.haciaAtrasToolStripMenuItem.Click += new System.EventHandler(this.haciaAtrasToolStripMenuItem_Click);
             // 
             // haciaAdelanteToolStripMenuItem
             // 
             this.haciaAdelanteToolStripMenuItem.Name = "haciaAdelanteToolStripMenuItem";
-            this.haciaAdelanteToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.haciaAdelanteToolStripMenuItem.Size = new System.Drawing.Size(229, 34);
             this.haciaAdelanteToolStripMenuItem.Text = "Hacia adelante";
             this.haciaAdelanteToolStripMenuItem.Click += new System.EventHandler(this.haciaAdelanteToolStripMenuItem_Click);
             // 
             // btnIr
             // 
-            this.btnIr.Location = new System.Drawing.Point(818, 39);
+            this.btnIr.Location = new System.Drawing.Point(908, 5);
             this.btnIr.Name = "btnIr";
-            this.btnIr.Size = new System.Drawing.Size(75, 23);
+            this.btnIr.Size = new System.Drawing.Size(101, 32);
             this.btnIr.TabIndex = 1;
             this.btnIr.Text = "Ir";
             this.btnIr.UseVisualStyleBackColor = true;
@@ -97,7 +99,7 @@
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "https://www.youtube.com"});
-            this.comboBox1.Location = new System.Drawing.Point(13, 37);
+            this.comboBox1.Location = new System.Drawing.Point(103, 8);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(799, 28);
             this.comboBox1.TabIndex = 2;
@@ -107,14 +109,29 @@
             this.webBrowser1.Location = new System.Drawing.Point(13, 71);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(1388, 547);
+            this.webBrowser1.Size = new System.Drawing.Size(109, 134);
             this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.Visible = false;
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView21.Location = new System.Drawing.Point(0, 36);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(1413, 594);
+            this.webView21.Source = new System.Uri("https://www.bing.com", System.UriKind.Absolute);
+            this.webView21.TabIndex = 4;
+            this.webView21.ZoomFactor = 1D;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1413, 630);
+            this.Controls.Add(this.webView21);
             this.Controls.Add(this.webBrowser1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.btnIr);
@@ -125,6 +142,7 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -140,6 +158,7 @@
         private System.Windows.Forms.Button btnIr;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.WebBrowser webBrowser1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
     }
 }
 
--- PaginaWeb/Form1.cs
+++ PaginaWeb/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
 
 namespace PaginaWeb
 {
@@ -15,26 +16,61 @@ namespace PaginaWeb
         public Form1()
         {
             InitializeComponent();
+            this.Resize += new System.EventHandler(this.Form_Resize);
+            webView21.NavigationStarting += EnsureHttps;
+            InitializeAsync();
+        }
+        async void InitializeAsync()
+        {
+            await webView21.EnsureCoreWebView2Async(null);
+            webView21.CoreWebView2.WebMessageReceived += UpdateAddressBar;
+
+            await webView21.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("window.chrome.webview.postMessage(window.document.URL);");
+            await webView21.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("window.chrome.webview.addEventListener(\'message\', event => alert(event.data));");
+        }
+        void UpdateAddressBar(object sender, CoreWebView2WebMessageReceivedEventArgs args)
+        {
+            String uri = args.TryGetWebMessageAsString();
+            comboBox1.Text = uri;
+            webView21.CoreWebView2.PostWebMessageAsString(uri);
+        }
+        void EnsureHttps(object sender, CoreWebView2NavigationStartingEventArgs args)
+        {
+            String uri = args.Uri;
+            if (!uri.StartsWith("https://"))
+            {
+                webView21.CoreWebView2.ExecuteScriptAsync($"alert('{uri} is not safe, try an https link')");
+                args.Cancel = true;
+            }
+        }
+
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            webView21.Size = this.ClientSize - new System.Drawing.Size(webView21.Location);
         }
 
         private void btnIr_Click(object sender, EventArgs e)
         {
+            //if (webView21 != null && webView21.CoreWebView2 != null)
+            //{
+            //    webView21.CoreWebView2.Navigate(comboBox1.Text);
+            //}
 
-            if (comboBox1.Text.Contains("https://www"+ ".com"))
+            if (comboBox1.Text.Contains("https://www."))
             {
-                webBrowser1.Navigate(new Uri(comboBox1.SelectedItem.ToString()));
+                webView21.CoreWebView2.Navigate(comboBox1.SelectedItem.ToString());
             }
             else if(comboBox1.Text.Contains(".com"))
             {
                 string busqueda = comboBox1.Text;
                 comboBox1.Text = "https://www." + busqueda;
-                webBrowser1.Navigate(new Uri(comboBox1.Text.ToString()));
+                webView21.CoreWebView2.Navigate(comboBox1.Text.ToString());
             }
             else
             {
                 string palabra = comboBox1.Text;
                 comboBox1.Text = "https://www.google.com/search?q=" + palabra;
-                webBrowser1.Navigate(new Uri(comboBox1.Text.ToString()));
+                webView21.CoreWebView2.Navigate(comboBox1.Text.ToString());
             }
 
 
@@ -44,23 +80,23 @@ namespace PaginaWeb
 
         private void inicioToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            webBrowser1.GoHome();
+            webView21.CoreWebView2.Navigate("https://www.bing.com");
         }
 
         private void haciaAtrasToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            webBrowser1.GoBack();
+            webView21.CoreWebView2.GoBack();
         }
 
         private void haciaAdelanteToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            webBrowser1.GoForward();
+            webView21.CoreWebView2.GoForward();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             comboBox1.SelectedIndex = 0;
-            webBrowser1.GoHome();
+           
         }
     }
 }
